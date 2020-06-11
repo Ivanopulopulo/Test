821ee6706ecd0c3dd3057dcfcd7179d3e57f4814
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2020-06-11 13:28:33 +0300

    Update ports.py

--- ports.py
+++ ports.py
@@ -2,16 +2,16 @@ import socket
 import threading
 
 def scan_port(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5) 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Создаем сокет
+    s.settimeout(0.5) #выставим тайм аут
     try:
-        con = s.connect((host, port))
-        print(host +": " + str(port) + " OPEN ")
+        con = s.connect((host, port)) #пробуем подключиться
+        print(host +": " + str(port) + " OPEN ") 
         con.close()
     except:
         pass
 host = input("Введите IP адресс или имя сайта: ")
-for i in range(9999):
+for i in range(9999): # проверим от 1 до 9999 порты
     p = threading.Thread(target=scan_port, args=(host,i)) #Потоки
     p.start()
 
